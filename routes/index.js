76c6188dc317caa024eae70a46842e1030844282
hectorcarreterito <hector.carreterito@gmail.com>
2015-05-22 13:26:07 +0200

    heroku

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
-
+//router.get('/author', quizController.author);
 module.exports = router;
